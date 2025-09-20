b44048fb77c56f729436cfa3f200dc701fcf6dda
Hamidullo <hamidullohikma@gmail.com>
2025-09-20 23:47:08 +0500

    Industry application done

---
+++ src/components/EcoScan/IndustryAplication/IndustryAplication.tsx
@@ -0,0 +1,13 @@
+import Text from "@/components/OfferSection.tsx/Text";
+import Aplication from "./Aplication";
+
+export default function IndustryAplication() {
+  return (
+    <section className="bg-[#05966936] py-12">
+        <Text title="Industry Applications" description=""/>
+        <div className="max-w-[1400px] m-auto">
+            <Aplication/>
+        </div>
+    </section>
+  )
+}
---
+++ src/constants/applications .ts
@@ -0,0 +1,45 @@
+import icon1 from '../assets/EcoScanLending/svg (6).png'
+import icon2 from '../assets/EcoScanLending/svg (7).png'
+import icon3 from '../assets/EcoScanLending/i (9).png'
+export const applications = [
+    {
+      id: 1,
+      icon:icon1,
+      title: "Agriculture",
+      description:
+        "Precision farming with crop health monitoring, yield prediction, and irrigation optimization",
+      points: [
+        "NDVI crop health analysis",
+        "Variable rate application maps",
+        "Irrigation management",
+      ],
+      bg: 'bg-gradient-to-r from-[#22C55E] to-[#10B981]'
+    },
+    {
+      id: 2,
+      icon: icon2,
+      title: "Construction",
+      description:
+        "Site planning, progress monitoring, and volumetric calculations for construction projects",
+      points: [
+        "Topographic surveying",
+        "Volume calculations",
+        "Progress tracking",
+      ],
+      bg: 'bg-gradient-to-r from-[#3B82F6] to-[#2563EB]'
+    },
+    {
+      id: 3,
+      icon: icon3,
+      title: "Environmental",
+      description:
+        "Ecosystem monitoring, conservation planning, and environmental impact assessment",
+      points: [
+        "Wildlife habitat mapping",
+        "Deforestation monitoring",
+        "Water quality assessment",
+      ],
+      bg: 'bg-gradient-to-r from-[#14B8A6] to-[#10B981]'
+    },
+  ]
+  
