417518d007b801c87948d47ed8b7bf403397814d
Hamidullo <hamidullohikma@gmail.com>
2025-09-21 00:57:34 +0500

    Capability section done

---
+++ src/components/CubeSat/Capability/Capability.tsx
@@ -0,0 +1,13 @@
+import Text from "@/components/OfferSection.tsx/Text";
+import CapabilityCard from "./CapabilityCard";
+
+export default function Capability() {
+  return (
+    <section className="bg-[#05966936] py-12">
+            <Text title="Data Outputs & Capabilities" description=""/>
+        <div className="max-w-[1400px] m-auto">
+            <CapabilityCard/>
+        </div>
+    </section>
+  )
+}
