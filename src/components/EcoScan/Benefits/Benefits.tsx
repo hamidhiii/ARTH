c1701ff25316dc295787d6dd3f723eece840ef95
Hamidullo <hamidullohikma@gmail.com>
2025-09-20 20:21:58 +0500

    Benefits section done

---
+++ src/components/EcoScan/Benefits/Benefits.tsx
@@ -0,0 +1,13 @@
+import Text from "@/components/OfferSection.tsx/Text";
+import BenefitsCard from "./BenefitsCard";
+
+export default function Benefits() {
+  return (
+    <section className="bg-[#05966936] py-12">
+        <div className="max-w-[1400px] m-auto bg-gradient-to-r from-[#10B9810D] to-[#3B82F60D py-12">
+        <Text title="Key Benefits" description=""/>
+            <BenefitsCard/>
+        </div>
+    </section>
+  )
+}
---
+++ src/constants/benefits.ts
@@ -0,0 +1,31 @@
+import icon1 from '../assets/EcoScanLending/i (6).png'
+import icon2 from '../assets/EcoScanLending/i (7).png'
+import icon3 from '../assets/HomeLending/i (1).png'
+
+
+export const Benefits = [
+    {
+        id: 1,
+        title: "Renewable Energy",
+        description: "Solar, wind, and sustainable power solutions",
+        icon: icon1,
+        bg: "bg-gradient-to-r from-[#10B981] to-[#10B981]",
+       
+    },
+    {
+        id: 2,
+        title: "Energy Efficiency",
+        description: "Smart systems reducing energy consumption",
+        icon: icon2,
+        bg: "bg-gradient-to-r from-[#3B82F6] to-[#06B6D4]",
+       
+    },
+    {
+        id: 3,
+        title: "Water Solutions",
+        description: "Purification and conservation technologies",
+        icon: icon3,
+        bg: "bg-gradient-to-r from-[#22C55E] to-[#10B981]",
+       
+    },
+]
